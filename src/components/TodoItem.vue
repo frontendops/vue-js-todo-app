b7341c9ba3c65ce87071d8c8b1ae4ab9dbffd701
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-03-24 19:06:51 -0400

    adding todos to state, removin and checking off

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  border-radius: 100px;
+  background-color: red;
+  color: white;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>TodoList</header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+header {
+  background-color: darkgray;
+  color: white;
+  font-size: 1.6em;
+}
+</style>
