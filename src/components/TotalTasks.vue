3be9ea6ae5dd61732cffbc77dfe3b9ae6fa4e5ba
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-03-29 19:41:45 -0400

    added conditional styles to circle chart

--- src/components/TotalTasks.vue
+++ src/components/TotalTasks.vue
@@ -9,7 +9,8 @@
           a 15.9155 15.9155 0 0 1 0 -31.831"
       ></path>
       <path
-        class="circle"
+        class="low"
+        v-bind:class="{ low: low, med: med, high: high}"
         v-bind:stroke-dasharray="stroke"
         d="M18 2.0845
       a 15.9155 15.9155 0 0 1 0 31.831
@@ -26,16 +27,53 @@ export default {
   props: ["total", "finished"],
   data() {
     return {
-      stroke: `${(this.finished / this.total).toFixed(2) * 100} 100`
+      stroke: `${(this.finished / this.total).toFixed(2) * 100} 100`,
+      low: true,
+      med: false,
+      high: false
     };
   },
   methods: {},
   watch: {
     finished: function() {
       this.stroke = `${(this.finished / this.total).toFixed(2) * 100} 100`;
+
+      if ((this.finished / this.total).toFixed(2) * 100 <= 33) {
+        this.low = true;
+        this.med = false;
+        this.high = false;
+      } else if (
+        (this.finished / this.total).toFixed(2) * 100 > 34 &&
+        (this.finished / this.total).toFixed(2) * 100 <= 68
+      ) {
+        this.low = false;
+        this.med = true;
+        this.high = false;
+      } else {
+        this.low = false;
+        this.med = false;
+        this.high = true;
+      }
     },
     total: function() {
       this.stroke = `${(this.finished / this.total).toFixed(2) * 100} 100`;
+
+      if ((this.finished / this.total).toFixed(2) * 100 <= 33) {
+        this.low = true;
+        this.med = false;
+        this.high = false;
+      } else if (
+        (this.finished / this.total).toFixed(2) * 100 > 34 &&
+        (this.finished / this.total).toFixed(2) * 100 <= 68
+      ) {
+        this.low = false;
+        this.med = true;
+        this.high = false;
+      } else {
+        this.low = false;
+        this.med = false;
+        this.high = true;
+      }
     }
   }
 };
@@ -48,19 +86,32 @@ export default {
   font-weight: 800;
   margin-bottom: 15px;
 }
-
 .task-container {
   margin-top: 40px;
 }
-
 .circular-chart {
   display: block;
   margin: 10px auto;
   max-width: 80%;
   max-height: 250px;
 }
+.low {
+  stroke: #ff9f00;
+  fill: none;
+  stroke-width: 2.8;
+  stroke-linecap: round;
+  animation: progress 1s ease-out forwards;
+}
 
-.circle {
+.med {
+  stroke: #3c9ee5;
+  fill: none;
+  stroke-width: 2.8;
+  stroke-linecap: round;
+  animation: progress 1s ease-out forwards;
+}
+
+.high {
   stroke: #4cc790;
   fill: none;
   stroke-width: 2.8;
@@ -74,13 +125,11 @@ export default {
   font-size: 0.5em;
   text-anchor: middle;
 }
-
 .circle-bg {
   fill: none;
   stroke: #eee;
   stroke-width: 3.8;
 }
-
 @keyframes progress {
   0% {
     stroke-dasharray: 0 100;
